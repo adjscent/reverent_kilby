c57443ce6decfdea317c859d79ab9fb108a21ad5
adjscent <jasonsohjsh@gmail.com>
2019-03-30 20:03:08 +0800

    feat: data cleaning

---
+++ data.py
@@ -0,0 +1,29 @@
+import pandas as pd 
+import numpy as np
+import os
+
+def dateparse(dates):
+    new_dates = []
+    for d in dates:
+        try:
+            if ("/" in d):
+                if len(d.split("/")[2])==4:
+                    new_dates.append(pd.datetime.strptime(d, '%d/%m/%Y'))
+                else:
+                    new_dates.append(pd.datetime.strptime(d,'%d/%m/%y').strftime('%d/%m/%Y'))
+            
+            if ("-") in d:
+                d = d[2:]
+                new_dates.append(pd.datetime.strptime(d, '%y-%m-%d'))
+        except:
+            new_dates.append(np.nan)
+    return new_dates
+
+
+period_data = pd.read_csv("data/Period.csv",parse_dates=['start_date','end_date'], date_parser=dateparse) 
+symptom_data = pd.read_csv("data/Symptom.csv",parse_dates=['date'], date_parser=dateparse) 
+user_data = pd.read_csv("data/User.csv",parse_dates=['dob'], date_parser=dateparse)
+
+period_data.dropna(inplace=True)
+symptom_data.dropna(inplace=True)
+
